ccdb86b86a0a3c276c3c3832341c33da4a87e755
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2024-12-19 22:36:36 +0200

    Add project files.

---
+++ Cadar_Raul_Lab11/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Cadar_Raul_Lab11.Data;
+
+
+namespace Cadar_Raul_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+    }
+}
---
+++ Cadar_Raul_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Cadar_Raul_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
